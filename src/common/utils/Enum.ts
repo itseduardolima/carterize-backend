a4232827ecb42b12c9b0da5c690defae466efd56
itseduardolima <eduardolima2417@gmail.com>
2024-08-28 14:26:21 -0400

    utils

---
+++ src/common/utils/Enum.ts
@@ -0,0 +1,17 @@
+export enum ProfileValue {
+  ADMIN_VALUE = 1,
+  CLIENT_VALUE = 2,
+}
+
+export enum VerifyCredentials {
+  verify_email = 'email',
+  verify_password = 'password',
+}
+
+export enum ValidType {
+  IS_NUMERIC = 'IS_NUMERIC',
+  IS_ALPHANUMERIC = 'IS_ALPHANUMERIC',
+  IS_ALPHABETIC = 'IS_ALPHABETIC',
+  ALL_CARACTERS = 'ALL_CARACTERS'
+}
+
