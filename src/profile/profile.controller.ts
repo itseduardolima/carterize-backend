62e9d5b983ba775e86094c3896e5543aaeef2d85
itseduardolima <eduardolima2417@gmail.com>
2024-08-28 14:23:57 -0400

    feat: Crud profile

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { PermissionGuard } from 'src/auth/shared/guards/permission.guard';
+import AccessProfile from 'src/auth/enums/permission.type';
+
+@ApiTags('profile')
+@Controller('profile')
+@ApiBearerAuth()
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Post()
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return this.profileService.create(createProfileDto);
+  }
+}
